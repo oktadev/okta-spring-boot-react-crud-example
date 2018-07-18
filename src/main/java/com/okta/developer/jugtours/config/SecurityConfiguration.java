83edd89c0d7f66ba7a2259295900d2b98c43b7c2
Matt Raible <matt.raible@okta.com>
2018-07-18 14:29:35 -0600

    Try implementing custom referrer request cache

--- src/main/java/com/okta/developer/jugtours/config/SecurityConfiguration.java
+++ src/main/java/com/okta/developer/jugtours/config/SecurityConfiguration.java
@@ -1,15 +1,33 @@
 package com.okta.developer.jugtours.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.PortResolver;
+import org.springframework.security.web.PortResolverImpl;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.savedrequest.DefaultSavedRequest;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Configuration
-@EnableOAuth2Sso
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private static final String SAVED_LOGIN_ORIGIN_URI = SecurityConfiguration.class.getName() + "_SAVED_ORIGIN";
+    private final Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -21,6 +39,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             .and()
+                .requestCache().requestCache(refererRequestCache())
+            .and()
                 .authorizeRequests()
                 .antMatchers("/", "/api/user").permitAll()
                 .anyRequest().authenticated();/*
@@ -28,5 +48,137 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .requiresChannel()
                 .requestMatchers(r -> r.getHeader("x-forwarded-proto") != null)
                 .requiresSecure();*/
+    }
+
+    @Bean
+    @Profile("dev")
+    public RequestCache refererRequestCache() {
+        return new RequestCache() {
+            private RequestMatcher requestMatcher = AnyRequestMatcher.INSTANCE;
+            private PortResolver portResolver = new PortResolverImpl();
+
+            @Override
+            public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+                if (request.getRemoteUser() == null && this.requestMatcher.matches(request)) {
+                    String referrer = request.getHeader("referer");
+                    if (!StringUtils.isEmpty(referrer) &&
+                            request.getSession().getAttribute(SAVED_LOGIN_ORIGIN_URI) == null) {
+                        log.info("Saving login origin URI: {}", referrer);
+                        SavedRequest savedRequest = referrerRequest(referrer);
+                        request.getSession().setAttribute(SAVED_LOGIN_ORIGIN_URI, savedRequest);
+                    }
+                } else {
+                    log.debug("Request not saved as configured RequestMatcher did not match");
+                }
+            }
+
+            @Override
+            public SavedRequest getRequest(HttpServletRequest request, HttpServletResponse response) {
+                HttpSession session = request.getSession(false);
+
+                if (session != null) {
+                    return (SavedRequest) session.getAttribute(SAVED_LOGIN_ORIGIN_URI);
+                }
+
+                return null;
+            }
+
+            @Override
+            public HttpServletRequest getMatchingRequest(HttpServletRequest request, HttpServletResponse response) {
+                DefaultSavedRequest saved = (DefaultSavedRequest) getRequest(request, response);
+
+                if (saved == null) {
+                    return null;
+                }
+
+                if (!saved.doesRequestMatch(request, portResolver)) {
+                    log.debug("saved request doesn't match");
+                    return null;
+                }
+
+                removeRequest(request, response);
+
+                return new SavedRequestAwareWrapper(saved, request);
+            }
+
+            @Override
+            public void removeRequest(HttpServletRequest request, HttpServletResponse response) {
+                HttpSession session = request.getSession(false);
+
+                if (session != null) {
+                    log.debug("Removing SavedRequest from session if present");
+                    session.removeAttribute(SAVED_LOGIN_ORIGIN_URI);
+                }
+            }
+        };
+    }
+
+    private static final TimeZone GMT_ZONE = TimeZone.getTimeZone("GMT");
+    private SavedRequest savedRequest = null;
+    /**
+     * The set of SimpleDateFormat formats to use in getDateHeader(). Notice that because
+     * SimpleDateFormat is not thread-safe, we can't declare formats[] as a static
+     * variable.
+     */
+    protected final SimpleDateFormat[] formats = new SimpleDateFormat[3];
+
+    class SavedRequestAwareWrapper extends HttpServletRequestWrapper {
+
+        SavedRequestAwareWrapper(SavedRequest saved, HttpServletRequest request){
+            super(request);
+            savedRequest = saved;
+
+            formats[0] = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
+            formats[1] = new SimpleDateFormat("EEEEEE, dd-MMM-yy HH:mm:ss zzz", Locale.US);
+            formats[2] = new SimpleDateFormat("EEE MMMM d HH:mm:ss yyyy", Locale.US);
+
+            formats[0].setTimeZone(GMT_ZONE);
+            formats[1].setTimeZone(GMT_ZONE);
+            formats[2].setTimeZone(GMT_ZONE);
+        }
+    }
+
+    private SavedRequest referrerRequest(final String referrer) {
+        return new SavedRequest() {
+            @Override
+            public String getRedirectUrl() {
+                return referrer;
+            }
+
+            @Override
+            public List<Cookie> getCookies() {
+                return null;
+            }
+
+            @Override
+            public String getMethod() {
+                return null;
+            }
+
+            @Override
+            public List<String> getHeaderValues(String name) {
+                return null;
+            }
+
+            @Override
+            public Collection<String> getHeaderNames() {
+                return null;
+            }
+
+            @Override
+            public List<Locale> getLocales() {
+                return null;
+            }
+
+            @Override
+            public String[] getParameterValues(String name) {
+                return new String[0];
+            }
+
+            @Override
+            public Map<String, String[]> getParameterMap() {
+                return null;
+            }
+        };
     }
 }
--- src/main/java/com/okta/developer/jugtours/web/UserController.java
+++ src/main/java/com/okta/developer/jugtours/web/UserController.java
@@ -1,11 +1,12 @@
 package com.okta.developer.jugtours.web;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2LoginAuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,53 +19,32 @@ import java.util.Map;
 @RestController
 public class UserController {
 
-    private final UserInfoRestTemplateFactory templateFactory;
-
     @Value("${spring.security.oauth2.client.provider.okta.issuer-uri}")
     String issuerUri;
 
-    public UserController(UserInfoRestTemplateFactory templateFactory) {
-        this.templateFactory = templateFactory;
-    }
-
     @GetMapping("/api/user")
-    @SuppressWarnings("unchecked")
     public ResponseEntity<?> getUser(Principal principal) {
         if (principal == null) {
             return new ResponseEntity<>("", HttpStatus.OK);
         }
-        if (principal instanceof OAuth2Authentication) {
-            OAuth2Authentication authentication = (OAuth2Authentication) principal;
-            Map<String, Object> details = (Map<String, Object>) authentication.getUserAuthentication().getDetails();
+        if (principal instanceof OAuth2LoginAuthenticationToken) {
+            OidcUser authentication = (OidcUser) principal;
+            Map<String, Object> details = authentication.getUserInfo().getClaims();
             return ResponseEntity.ok().body(details);
         } else {
             return ResponseEntity.ok().body(principal.getName());
         }
     }
 
-    @GetMapping("/api/tokens")
-    public ResponseEntity<Map<String, String>> getTokens() {
-        OAuth2RestTemplate oauth2RestTemplate = this.templateFactory.getUserInfoRestTemplate();
-        String idToken = (String) oauth2RestTemplate.getAccessToken().getAdditionalInformation().get("id_token");
-
-        Map<String, String> tokens = new HashMap<>();
-        tokens.put("access_token", oauth2RestTemplate.getAccessToken().getValue());
-        tokens.put("id_token", idToken);
-        return ResponseEntity.ok(tokens);
-    }
-
     @PostMapping("/api/logout")
-    public ResponseEntity<?> logout(HttpServletRequest request) {
+    public ResponseEntity<?> logout(HttpServletRequest request,
+                                    @AuthenticationPrincipal(expression = "idToken") OidcIdToken idToken) {
         // send logout URL to client so they can initiate logout - doesn't work from the server side
-
-        OAuth2RestTemplate oauth2RestTemplate = this.templateFactory.getUserInfoRestTemplate();
-        String idToken = (String) oauth2RestTemplate.getAccessToken().getAdditionalInformation().get("id_token");
-
         String logoutUrl = issuerUri + "/v1/logout";
 
         Map<String, String> logoutDetails = new HashMap<>();
         logoutDetails.put("logoutUrl", logoutUrl);
-        logoutDetails.put("idToken", idToken);
+        logoutDetails.put("idToken", idToken.getTokenValue());
         request.getSession(false).invalidate();
         return ResponseEntity.ok().body(logoutDetails);
     }
