9aab5daecbcf01772a680767a77895d5163d92f3
fatcatdog <duchenjacob@gmail.com>
2020-02-20 20:27:18 -0500

    In  in GroupEdit/handleSubmit function - Fixing edit group fetch + utilizing unused variable (#13)
    * fixed buggy put fetch issue
    
    * in previous commit, also utilized the csrftoken that was taken from state but not used previously

--- app/src/GroupEdit.js
+++ app/src/GroupEdit.js
@@ -54,10 +54,10 @@ class GroupEdit extends Component {
     event.preventDefault();
     const {item, csrfToken} = this.state;
 
-    await fetch('/api/group', {
+    await fetch('/api/group' + (item.id ? '/' + item.id : '') , {
       method: (item.id) ? 'PUT' : 'POST',
       headers: {
-        'X-XSRF-TOKEN': this.state.csrfToken,
+        'X-XSRF-TOKEN': csrfToken,
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
